053dd7e7479ba47095a6706c579945bad48907c6
falcaodiogo <falcao.diogo@ua.pt>
2024-03-12 23:03:59 +0000

    heart rate!

--- wear_counter-main/android/app/src/main/kotlin/com/example/punch_timetek/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nyorok.counter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ wear_main/android/app/src/main/kotlin/com/example/wear_main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wear_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
