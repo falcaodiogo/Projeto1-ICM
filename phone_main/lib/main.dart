9289fba2d87c22eeb1d17679b4c5f9b7ee33862b
falcaodiogo <falcao.diogo@ua.pt>
2024-03-13 00:23:34 +0000

    Heart Rate no relógio!

--- phone_main/lib/main.dart
+++ phone_main/lib/main.dart
@@ -1,110 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:workout/workout.dart';
+import 'package:phone_main/widgets/mqttView.dart';
+import 'package:phone_main/mqtt/state/MQTTAppState.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final workout = Workout();
-
-  final exerciseType = ExerciseType.walking;
-  final features = [
-    WorkoutFeature.heartRate,
-    WorkoutFeature.calories,
-    WorkoutFeature.steps,
-    WorkoutFeature.distance,
-    WorkoutFeature.speed,
-  ];
-  final enableGps = true;
-
-  double heartRate = 0;
-  double calories = 0;
-  double steps = 0;
-  double distance = 0;
-  double speed = 0;
-  bool started = false;
-
-  _MyAppState() {
-    workout.stream.listen((event) {
-      // ignore: avoid_print
-      print('${event.feature}: ${event.value} (${event.timestamp})');
-      switch (event.feature) {
-        case WorkoutFeature.unknown:
-          return;
-        case WorkoutFeature.heartRate:
-          setState(() {
-            heartRate = event.value;
-          });
-          break;
-        case WorkoutFeature.calories:
-          setState(() {
-            calories = event.value;
-          });
-          break;
-        case WorkoutFeature.steps:
-          setState(() {
-            steps = event.value;
-          });
-          break;
-        case WorkoutFeature.distance:
-          setState(() {
-            distance = event.value;
-          });
-          break;
-        case WorkoutFeature.speed:
-          setState(() {
-            speed = event.value;
-          });
-          break;
-      }
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /*
+    final MQTTManager manager = MQTTManager(host:'test.mosquitto.org',topic:'flutter/amp/cool',identifier:'ios');
+    manager.initializeMQTTClient();
+
+     */
+
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
-      // Use ambient mode to stay alive in the foreground
-      // Use a foreground service if you want to stay alive in the background
-      home: const Text("here")
-          );
-  }
-
-  void toggleExerciseState() async {
-    setState(() {
-      started = !started;
-    });
-
-    if (started) {
-      final supportedExerciseTypes = await workout.getSupportedExerciseTypes();
-      // ignore: avoid_print
-      print('Supported exercise types: ${supportedExerciseTypes.length}');
-
-      final result = await workout.start(
-        // In a real application, check the supported exercise types first
-        exerciseType: exerciseType,
-        features: features,
-        enableGps: enableGps,
-      );
-
-      if (result.unsupportedFeatures.isNotEmpty) {
-        // ignore: avoid_print
-        print('Unsupported features: ${result.unsupportedFeatures}');
-        // In a real application, update the UI to match
-      } else {
-        // ignore: avoid_print
-        print('All requested features supported');
-      }
-    } else {
-      await workout.stop();
-    }
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider<MQTTAppState>(
+        create: (_) => MQTTAppState(),
+        child: Scaffold(
+          body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                    child: MQTTView(),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
