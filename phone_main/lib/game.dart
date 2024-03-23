0b7999851c10859b3b9cbe16bc2cc0921f53542b
falcaodiogo <falcao.diogo@ua.pt>
2024-03-23 19:53:06 +0000

    main game file

--- phone_main/lib/game.dart
+++ phone_main/lib/game.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:phone_main/widgets/appbar.dart';
+import 'package:phone_main/widgets/columngame.dart';
 
+// ignore: use_key_in_widget_constructors
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page 2'),
-      ),
-      body: Center(
-        child: Text('This is Page 2'),
+    return Container(
+      color: const Color.fromARGB(255, 19, 35, 44),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            appBar(context),
+            columnGame(context),
+          ],
+        ),
       ),
     );
   }
